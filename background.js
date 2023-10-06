540329ebfc1c57745f379804566e901a1057460b
vivekmth79 <122387680+vivekmth79@users.noreply.github.com>
2023-10-06 18:57:32 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,20 @@
 // console.log("This is background Script");
-/*
-    Author: Arijit Paria 
-    Subscribe @tutoriex on youtube to get more such scripts
-    Note:
-    This script is free to use, do not pay anyone anything.
-    To modify or redistribute, kindly follow the license agreement strictly.
-*/
-console.log("This is background Script @tutoriex (Arijit Paria)");
 
-let installURL = "https://www.youtube.com/@Tutoriex_?sub_confirmation=1";
+console.log("This is background Script ");
+
+let installURL = "";
 chrome.runtime.onInstalled.addListener(function (details) {
   // console.log(details);
   if (details.reason === "install") {
     chrome.tabs.create({
-      url: "https://github.com/arijitparia2002",
+      url: "",
     });
     chrome.tabs.create({
       url: installURL,
     });
     chrome.notifications.create({
       title: "TUTORIEX",
-      message: "Please Subscribe to TUTORIEX YouTube Channel.",
+      message: "suggest changes",
       iconUrl: "icon.png",
       type: "basic",
     });
